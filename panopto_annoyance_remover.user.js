8c685ee2c99fc781cb6c39e1c9e4a97fa630539b
jaytohe <antolbestg4@gmail.com>
2021-02-09 17:51:59 +0000

    New and improved method for patching listeners.; Prevent scrolling of sidebar; Re-enable shortucts.

--- panopto_annoyance_remover.user.js
+++ panopto_annoyance_remover.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Panopto Annoyances Remover
 // @namespace    http://github.com/jaytohe/
-// @version      1.1.1
+// @version      1.2.0
 // @description  Fixes many annoyances in Panopto. Space button now always controls the video.
 // @author       jaytohe
 // @match        https://*.panopto.eu/Panopto/Pages/Viewer.aspx?id=*
@@ -12,12 +12,7 @@
 (function() {
     'use strict';
     const BlackListIDs = ['quickRewindButton','quickFastForwardButton','volumeControl', 'captionsButton', 'playButton', 'playSpeedButton', 'qualityButton', 'captionStyleOptions'];
-    document.addEventListener("keydown", function(event) {
-        event.stopImmediatePropagation();
-        if (event.keyCode == 32) {
-            document.getElementById('playButton').click(); //click playbtn.
-        }
-    }, false);
+
     function checkID(val) {
      if (typeof val !== 'undefined') {
          const thumb_regex = /thumbnail\d+thumbnailList/g;
@@ -36,11 +31,22 @@
     if (EventTarget.prototype.original_addEventListener == null) {
         EventTarget.prototype.original_addEventListener = EventTarget.prototype.addEventListener;
         EventTarget.prototype.addEventListener =function override_listener(typ, fn, opt) {
-            if ( !checkID(this.id)
-                || (checkID(this.id) && typ !== 'keydown')
-               ) {
-                    this.original_addEventListener(typ, fn, opt);
-                 }
+
+            if (checkID(this.id) && typ === 'keydown') {
+                const tmp_func = function(e) {
+                    const original = fn;
+                    if (e.keyCode === 32) {
+                        e.stopImmediatePropagation();
+                        e.preventDefault(); //prevent scrolling of sidebar.
+                        document.getElementById('playButton').click();
+                        return;
+                    }
+                    original(e);
+                }
+                this.original_addEventListener(typ, tmp_func, opt);
             }
+
+            this.original_addEventListener(typ, fn, opt);
+        }
         }
 })();
